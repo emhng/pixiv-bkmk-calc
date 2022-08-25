e1aec090b5ab07586bd2ab934c57ab2218657d61
emhng <contactemilyhuang@gmail.com>
2022-08-25 13:48:35 -0700

    Add calculations to include likes if toggled

--- components/BookmarkCalc.tsx
+++ components/BookmarkCalc.tsx
@@ -2,43 +2,94 @@ import { useState } from 'react';
 
 import { EyeIcon } from '@heroicons/react/24/solid';
 import { HeartIcon } from '@heroicons/react/24/solid';
+import { FaceSmileIcon } from '@heroicons/react/24/solid';
+
+import { useRouter } from 'next/router';
 
 const BookmarkCalc = () => {
 	const [viewCount, setViewCount] = useState('');
 	const [bookmarkCount, setBookmarkCount] = useState('');
 	const [likeCount, setLikeCount] = useState('');
 
-	const rawPercent: number = (+bookmarkCount / +viewCount) * 100;
-	const roundedPercent: number = Math.round(rawPercent * 10) / 10;
+	const { query } = useRouter();
+	const includeLikes = !!query.likes;
 
-	const onChangeHandler = (setStateFn: any) => (
+	const roundDecimal = (percent: number) => Math.round(percent * 10) / 10;
+
+	const rawPercent: number = (+bookmarkCount / +viewCount) * 100;
+	const roundedPercent: number = roundDecimal(rawPercent);
+
+	const rawPercentWithLikes: number =
+		((+bookmarkCount + +likeCount) / +viewCount) * 100;
+
+	const roundedPercentWithLikes: number = roundDecimal(rawPercentWithLikes);
+
+	const onChangeHandler = (setStateFn: CallableFunction) => (
 		e: React.ChangeEvent<HTMLInputElement>
 	) => {
 		setStateFn(e.target.value);
 	};
 
+	let result: number | string;
+	let likesInput = [];
+
+	if (includeLikes === true) {
+		result =
+			(includeLikes === true && viewCount === '') ||
+			(includeLikes === true && bookmarkCount === '') ||
+			(includeLikes === true && likeCount === '')
+				? '--'
+				: roundedPercentWithLikes;
+
+		likesInput.push(
+			<div className="vflex">
+				<label>
+					<FaceSmileIcon className="icon" />
+					いいね
+				</label>
+				<input
+					onChange={onChangeHandler(setLikeCount)}
+					type="number"
+					min={0}
+					value={likeCount}
+				/>
+			</div>
+		);
+	} else {
+		result = viewCount === '' || bookmarkCount === '' ? '--' : roundedPercent;
+	}
+
 	return (
-		<div className="calc-cont hflex">
+		<div className="calc-cont vflex">
 			<form>
 				<div className="vflex">
 					<label>
 						<EyeIcon className="icon" />
 						閲覧数
 					</label>
-					<input onChange={onChangeHandler(setViewCount)} type="number" />
+					<input
+						onChange={onChangeHandler(setViewCount)}
+						type="number"
+						min={1}
+					/>
 				</div>
 				<div className="vflex">
 					<label>
 						<HeartIcon className="icon" />
 						ブックマーク
 					</label>
-					<input onChange={onChangeHandler(setBookmarkCount)} type="number" />
+					<input
+						onChange={onChangeHandler(setBookmarkCount)}
+						type="number"
+						min={0}
+					/>
 				</div>
+				{likesInput}
 			</form>
 			<div className="result">
 				<h1>ブクマ率</h1>
 				<h1>
-					{viewCount === '' || bookmarkCount === '' ? '--' : roundedPercent}
+					{result}
 					<span className="counter">%</span>
 				</h1>
 			</div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,26 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
+import Link from 'next/link';
+
+import { useRouter } from 'next/router';
+
 import BookmarkCalc from '../components/BookmarkCalc';
 
+import { useState } from 'react';
+
 const Home: NextPage = () => {
+	const { query } = useRouter();
+	const includeLikes = !!query.likes;
+
+	const [calculators, setCalculators] = useState([<BookmarkCalc key={1} />]);
+
+	const addCalc = () =>
+		setCalculators([
+			...calculators,
+			<BookmarkCalc key={calculators.length + 1} />
+		]);
+
 	return (
 		<div id="page-cont">
 			<Head>
@@ -13,10 +30,16 @@ const Home: NextPage = () => {
 				<meta name="description" content="pixivブクマ率計算ツール" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			<BookmarkCalc />
-			<BookmarkCalc />
-			<BookmarkCalc />
+			<h1>pixivブクマ率計算ツール</h1>
+			<Link href={includeLikes ? '/' : '/?likes=true'}>
+				<a className="link-button">
+					{includeLikes ? 'いいねを除く' : 'いいねを含む'}
+				</a>
+			</Link>
+			<section className="hflex">
+				{includeLikes ? calculators : calculators}
+				<button onClick={addCalc}>Add another calculator</button>
+			</section>
 		</div>
 	);
 };
