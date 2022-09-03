f95e92abae3dde05a66b5c8b873616f62dcca610
emhng <contactemilyhuang@gmail.com>
2022-09-02 18:36:01 -0700

    Configure for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  basePath: '/pixiv-bkmk-calc'
 }
-
-module.exports = nextConfig
