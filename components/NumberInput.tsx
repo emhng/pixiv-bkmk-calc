11931fa7d4bf265169041a45937e5a2ebcd61228
emhng <contactemilyhuang@gmail.com>
2022-09-05 22:40:15 -0700

    Adjust styling for mobile

--- components/NumberInput.tsx
+++ components/NumberInput.tsx
@@ -1,38 +1,39 @@
 const NumberInput = ({
-	id,
-	icon,
-	label,
-	onChangeHandler,
-	onInputHandler,
-	min,
-	errorState,
-	value
+  id,
+  icon,
+  label,
+  onChangeHandler,
+  onInputHandler,
+  min,
+  errorState,
+  value
 }: {
-	id: string;
-	icon: any;
-	label: string;
-	onChangeHandler: any;
-	onInputHandler: any;
-	min: number;
-	errorState: any;
-	value?: string;
+  id: string
+  icon: any
+  label: string
+  onChangeHandler: any
+  onInputHandler: any
+  min: number
+  errorState: any
+  value?: string
 }) => {
-	return (
-		<div className="vflex input-cont">
-			<label htmlFor={id}>
-				{icon}
-				{label}
-			</label>
-			<input
-				onChange={onChangeHandler}
-				onInput={onInputHandler}
-				type="number"
-				min={min}
-				id={id}
-				value={value}
-			/>
-			<div>{errorState}</div>
-		</div>
-	);
-};
-export default NumberInput;
+  return (
+    <div className='vflex input-cont'>
+      <label htmlFor={id}>
+        {icon}
+        {label}
+      </label>
+      <input
+        onChange={onChangeHandler}
+        onInput={onInputHandler}
+        type='number'
+        inputMode='numeric'
+        min={min}
+        id={id}
+        value={value}
+      />
+      <div>{errorState}</div>
+    </div>
+  )
+}
+export default NumberInput
